f8937cc93b62b75958272bb3ac974c7c1a46f314
Kravets_Dmytro <ditur@ukr.net>
2023-03-30 21:50:46 +0300

    перше дз з уроку 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}" style="border-radius: 15px">
+  <img
+		class="gallery__image"
+		src="${preview}"
+		alt="${description}"
+		style="border-radius: 15px" />
+</a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {    captionsData: 'alt',
+captionDelay: 300,});
+
+
+
