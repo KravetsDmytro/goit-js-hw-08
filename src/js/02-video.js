170ed93f2dc21c8b03760c44828ff1efbbd7ded4
Kravets_Dmytro <ditur@ukr.net>
2023-04-03 10:39:21 +0300

    tz2 commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,39 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-
-const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-  localStorage.setItem(STORAGE_KEY, seconds);
+function onPlay({ evt }) {
+  const currentTime = evt.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTime);
 }
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+const savedTime = localStorage.getItem(STORAGE_KEY);
+console.log(savedTime);
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//   // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//   switch (error.name) {
+//       case 'RangeError':
+//           // the time was less than 0 or greater than the video’s duration
+//           break;
+
+//       default:
+//           // some other error occurred
+//           break;
+//   }
+// });
